67c7a6de5f8d37962e733a31c5af7fbca20cbd66
AmilKahrovic06 <114291366+AmilKahrovic06@users.noreply.github.com>
2022-11-25 16:10:35 +0100

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,88 @@
+// String
+// Number
+// Primitivni
+// String
+// Number
+// Boolean
+// undefined
+// null
+// Symbol
+//primitivni(gore)
+
+// referentni:
+// Array
+// Object
+// function
+/*
+*/
+tekst="Hello World";
+console.log(tekst);
+
+//Number
+broj=25;
+console.log(broj);
+
+console.log(typeof(tekst));
+console.log(typeof(broj));
+
+//Boolean-true ili false
+polozenHTML=true;
+console.log(polozenHTML);
+imePrezime="Amil Kahrovic";
+
+
+//Undefined
+// age;
+// console.log(age);
+
+
+let nekitekst=null;
+console.log(nekitekst);
+
+ime="Sucro";
+prezime="Hajrovic";
+
+imePrezime=ime + " " + prezime;
+console.log(imePrezime);
+
+noviTekst=ime+5;
+
+//Logicki operatori
+
+//&&-i operator
+//||-ili
+//!-negacija
+console.log("--------------i----------------");
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+//ili-||
+console.log("--------------ili----------------");
+console.log( true || true);
+console.log(true ||false);
+console.log(false ||false);
+
+console.log("--------------negacija----------------");
+console.log(!true);
+console.log(!false);
+
+//Poredjenje
+console.log("--------------poredjenje----------------");
+prviBroj=10;
+drugiBroj=15;
+console.log(prviBroj==drugiBroj);
+//===
+console.log("--------------===----------------");
+console.log(prviBroj===drugiBroj);
+//!= 
+console.log("--------------!=----------------");           
+console.log(prviBroj!=drugiBroj);
+// !==
+console.log("--------------!==----------------");
+console.log(prviBroj!==drugiBroj);
+
+
+
