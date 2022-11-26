a8941fd11cbecf1a90d55c52e2d665bdba247c38
AmilKahrovic06 <114291366+AmilKahrovic06@users.noreply.github.com>
2022-11-26 20:22:21 +0100

    added vezba2

---
+++ vezba2/app.js
@@ -0,0 +1,67 @@
+console.log("123");
+ime= "Sucro";
+console.log(ime);
+
+prviBroj=10;
+console.log(prviBroj);
+prviBroj=prviBroj+1;
+console.log(prviBroj);
+prviBroj++;
+console.log(prviBroj);
+
+prviBroj--;
+console.log(prviBroj);
+// prviBroj=prviBroj+10;
+// console.log(prviBroj);
+// prviBroj+=10;
+// console.log(prviBroj);
+
+// prviBroj*=10;
+// prviBroj-=10;
+// prvibroj/=10;
+// prviBroj+=10;
+
+console.log(14%2);
+
+// if(uslov){
+// ako je uslov ispunjen
+// }else{
+// ako uslov nije ispunjen
+// }
+a=-10;
+if(a>0){
+    console.log("Broj je pozitivan");
+}else{
+    console.log("Broj je negativan");
+}
+
+ime="Pemba";
+if (ime==="Pemba"){
+    console.log("Zdravo,",ime);
+}else{
+    console.log("Zdravo,neznance,ja samo pembu poznajem");
+}
+
+
+
+//Zadatak 1
+//Inicijalizovati broj i proveriti jel pozitivan ili negativan
+
+nekiTamoBroj=0;
+if(nekiTamoBroj>0){
+    console.log("Broj je pozitivan");
+}else{
+    console.log("Broj je negativan");
+}
+//Uneti dva broja i proveriti da li su ta dva broja jednaka
+console.log("----------------------------------------------------------------");
+m=1;
+n="1";
+if(m===n){
+    console.log("Brojevi su jednaki");
+}else{
+    console.log("Brojevi nisu jednaki");
+}
+console.log("----------------------------------------------------------------");
+
+
