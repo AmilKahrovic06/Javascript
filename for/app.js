ccf6b803d60e796515eeb61f0a7b93e626f9fc56
AmilKahrovic06 <114291366+AmilKahrovic06@users.noreply.github.com>
2022-12-03 14:45:48 +0100

    added for loop

---
+++ for/app.js
@@ -0,0 +1,34 @@
+// for(i=0;i<5;i++){
+// console.log(i);
+// }
+
+//Naci sumu prvih 10 prirodnih brojeva
+// suma=0;
+// for(i=1;i<=10;i++){
+//     suma+=i;
+// }
+// console.log(suma);
+
+//Izracunati proizvod prvih 5 prirodnih brojeva
+// pr=1;
+// for(i=1;i<=5;i++){
+//     pr*=i;
+// }
+// console.log(pr);
+
+// uneti broj n i sabrati sumu od n do 1
+//uneti 10 brojeva sa tastaturre i naci sumu i srednju vrednost
+// suma=0;
+// a=0;
+// a=Number(prompt("Unesite broj sa kojim zelite da ispistete"));
+// for(i=1;i<=10;i++){
+//     console.log(a*i);
+// }
+//napraviti trougao od *
+zvezdice=" ";
+for(i=0;i<5;i++){
+   zvezdice+="*";
+   console.log(zvezdice);
+}
+
+
