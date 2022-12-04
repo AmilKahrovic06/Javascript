fd6f431e8bef66ade2766b619f6183865925e524
AmilKahrovic06 <114291366+AmilKahrovic06@users.noreply.github.com>
2022-12-04 18:28:57 +0100

    added number triangles

--- for/app.js
+++ for/app.js
@@ -25,10 +25,50 @@
 //     console.log(a*i);
 // }
 //napraviti trougao od *
-zvezdice=" ";
-for(i=0;i<5;i++){
-   zvezdice+="*";
-   console.log(zvezdice);
-}
+// zvezdice=" ";
+// for(i=0;i<5;i++){
+//    zvezdice+="*";
+//    console.log(zvezdice);
+// }
+
+//Domaci:
+// Write a program in C to display the pattern like right angle triangle with a number
+
+// The pattern like :
+
+// 1
+// 12
+// 123
+// 1234
+broj=1;
+h=2
+ for(i=1;i<=5;i++){
+    debugger;
+    console.log(broj);
+    broj=broj*10+h;
+    h++;
+ }
+
+ console.log("------------------------------------------------------");
+//  Write a program in C to make such a pattern like right angle triangle with a number which will repeat a number in a row.
+
+// The pattern like :
+
+//  1
+//  22
+//  333
+//  4444
+
+
+ n=1
+  for(i=1;i<=5;i++){
+     for(j=1;j<=i;j++){
+         console.log(i);
+     }
+     console.log("\n");
+  }
+
+
+
 
 
