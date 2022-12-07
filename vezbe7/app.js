50d2ced5303e29d934a7a284d9b90b820d06df39
AmilKahrovic06 <114291366+AmilKahrovic06@users.noreply.github.com>
2022-12-07 19:12:39 +0100

    added vezba7

---
+++ vezbe7/app.js
@@ -0,0 +1,66 @@
+//Proveriti da le je neki broj prost
+// n=5;
+// brojac=0;
+// for(i=1;i<n;i++){
+//     if(n%i==0){
+//      brojac++;
+//     }
+    
+// }
+// if(brojac>2){
+//     console.log("Ovo je nije prost broj.");
+// }else{
+// console.log("Ovo je prost broj.");
+// }
+
+//Proveriti da li je uneti broj savrsen
+// n=10;
+// suma=0;
+// for(i=1;i<n/2;i++){
+// if(n%i===0){
+//     suma+=i;
+// }
+// }
+// if(n===suma){
+//     console.log("Broj je savrsen.");
+// }else{
+//     console.log("Broj nije savrsen");
+// }
+
+// H e l l o  W o r l d
+// 0 1 2 3 4 5 6 7 8 9 10
+// text="Hello World";
+// console.log(text[7]);
+// console.log(text);
+
+// noviText=text+" Sucro";
+// console.log(noviText);
+// console.log(text.length);
+
+// t="";
+// for(i=0;i<text.length;i++){
+//     if(text[i]===" "){
+//         continue;
+//     }
+//     // console.log(text[i]);
+//     t+=text[i];
+// }
+// console.log(t);
+
+
+//proveriti da li su dva stringa ista
+
+str1="qwertz";
+str2="asdfgh";
+if(str1==str2){
+    console.log("Stringovi su isti");
+}else{
+    console.log("Stringovi su razliciti");
+}
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+
+noviString="       Text";
+
+console.log(noviString.trim());
+
