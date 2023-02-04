fdf489669bbb48f3883d0b99bec83c11b416e0a2
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-02-04 12:53:19 +0100

    added vezbe 23

--- vezbe23/app.js
+++ vezbe23/app.js
@@ -53,12 +53,65 @@ console.log(wordReverse(stringg));
 //var
 //Ukloniti elemente odredjene iz niza
 var nizElemenata = ["Neki", "Tamo", "Element"];
-var Ukloniti = "Tamo";
+var Ukloniti = ["Tamo", "Neki"];
 function UkloniElement(niz) {
-  return niz.filter((x) => x != Ukloniti);
+  return niz.filter((x) => x != Ukloniti[0] && x != Ukloniti[1]).join(" ");
 }
 console.log(UkloniElement(nizElemenata));
 
 //Za domaci ovaj zadatak preko slice i indexOf sa vise reci koje treba da se izbace
 
 //2.zadatak:
+const ucenici = [
+  {
+    ime: "Lejla",
+    Bodovi: [99, 80, 75],
+  },
+  {
+    ime: "Nejla",
+    Bodovi: [90, 60, 20],
+  },
+  {
+    ime: "Lajla",
+    Bodovi: [60, 80, 50],
+  },
+  {
+    ime: "Sara",
+    Bodovi: [10, 80, 20],
+  },
+  {
+    ime: "Mara",
+    Bodovi: [9, 0, 3],
+  },
+  {
+    ime: "Zara",
+    Bodovi: [99, 80, 70],
+  },
+  {
+    ime: "Hajra",
+    Bodovi: [99, 99, 99],
+  },
+  {
+    ime: "Sefketa",
+    Bodovi: [99, 85, 95],
+  },
+  {
+    ime: "Mulekelefeleketa",
+    Bodovi: [100, 100, 100],
+  },
+];
+
+function NajveciBrojBodova(Objekt) {
+  let suma = 0;
+  for (let i = 0; i < Objekt.length; i++) {
+    if (Objekt[i].Bodovi.reduce((p, t) => p + t) / 3 > 90) {
+      console.log(
+        Objekt[i].ime,
+        ",",
+        Objekt[i].Bodovi.reduce((p, t) => p + t) / 3
+      );
+    }
+  }
+}
+console.log("Ucenici sa visokim brojem bodova (preko 90)su:");
+NajveciBrojBodova(ucenici);
