27c91b6e11c2c6618421694e0820b9e40747246f
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-02-22 19:18:08 +0100

    updated app.js

--- vezbe27 (callbacks-asinhroni javascript-vezbanje)/app.js
+++ vezbe27 (callbacks-asinhroni javascript-vezbanje)/app.js
@@ -186,3 +186,129 @@ function objectFilter(obj, callback) {
   }
   return newObj;
 }
+
+// const cities = {
+// London: 'LONDON',
+// LA: 'Los Angeles',
+// Paris: 'PARIS',
+// };
+// console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
+
+// Challenge 12
+function majority(array, callback) {}
+
+// /*** Uncomment these to check your work! ***/
+// const isOdd = function(num) { return num % 2 === 1; };
+// console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+
+// Challenge 13
+function prioritize(array, callback) {
+  let niz1 = [];
+  let niz2 = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      niz1.push(array[i]);
+    } else {
+      niz2.push(array[i]);
+    }
+  }
+  return niz1.concat(niz2);
+}
+
+// /*** Uncomment these to check your work! ***/
+const startsWithS = function (str) {
+  return str[0] === "s" || str[0] === "S";
+};
+console.log(
+  prioritize(
+    ["curb", "rickandmorty", "seinfeld", "sunny", "friends"],
+    startsWithS
+  )
+); // should log:
+//['seinfeld', 'sunny', 'curb', 'rickandmorty', 'friends']
+
+// Challenge 14
+function countBy(array, callback) {
+  let obj = {};
+  for (let i = 0; i < array.length; i++) {
+    let kljuc = callback(array[i]);
+    if (obj[kljuc]) {
+      obj[kljuc] += 1;
+    } else {
+      obj[kljuc] = 1;
+    }
+  }
+  return obj;
+}
+
+// /*** Uncomment these to check your work! ***/
+console.log(
+  countBy([1, 2, 3, 4, 5], function (num) {
+    if (num % 2 === 0) return "even";
+    else return "odd";
+  })
+); // should log: { odd: 3, even: 2 }
+
+// Challenge 15
+function groupBy(array, callback) {
+  let obj = {};
+  for (let i = 0; i < array.length; i++) {
+    let kljuc = callback(array[i]);
+    if (obj[kljuc]) {
+      obj[kljuc].push(array[i]);
+    } else {
+      obj[kljuc] = [array[i]];
+    }
+  }
+  return obj;
+}
+
+// /*** Uncomment these to check your work! ***/
+const decimals = [1.3, 2.1, 2.4, 2.5, 2.6, 2.9];
+const floored = function (num) {
+  return Math.floor(num);
+};
+console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+// Challenge 16
+function goodKeys(obj, callback) {
+  let niz1 = [];
+  for (let el in obj) {
+    if (callback(obj[el])) {
+      niz1.push(el);
+    }
+  }
+  return niz1;
+}
+
+// /*** Uncomment these to check your work! ***/
+const sunny = {
+  mac: "priest",
+  dennis: "calculating",
+  charlie: "birdlaw",
+  dee: "bird",
+  frank: "warthog",
+};
+const startsWithBird = function (str) {
+  return str.slice(0, 4).toLowerCase() === "bird";
+};
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+
+// Challenge 17
+function commutative(func1, func2, value) {
+  let a = func1(value);
+  let b = func2(a);
+  let c = func2(value);
+  let d = func1(value);
+
+  return a == d;
+}
+
+// /*** Uncomment these to check your work! ***/
+const multBy3 = (n) => n * 3;
+const divBy4 = (n) => n / 4;
+const subtract5 = (n) => n - 5;
+console.log(commutative(multBy3, divBy4, 11)); // should log: true
+console.log(commutative(multBy3, subtract5, 10)); // should log: false
+console.log(commutative(divBy4, subtract5, 48)); // should log: false
