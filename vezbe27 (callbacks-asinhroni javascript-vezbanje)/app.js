8f33139ce81669b75596f4b8ba1027168ce17073
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-02-26 16:21:34 +0100

    updated app.js

--- vezbe27 (callbacks-asinhroni javascript-vezbanje)/app.js
+++ vezbe27 (callbacks-asinhroni javascript-vezbanje)/app.js
@@ -312,3 +312,106 @@ const subtract5 = (n) => n - 5;
 console.log(commutative(multBy3, divBy4, 11)); // should log: true
 console.log(commutative(multBy3, subtract5, 10)); // should log: false
 console.log(commutative(divBy4, subtract5, 48)); // should log: false
+
+// Challenge 18
+function objFilter(obj, callback) {}
+
+// /*** Uncomment these to check your work! ***/
+// const startingObj = {};
+// startingObj[6] = 3;
+// startingObj[2] = 1;
+// startingObj[12] = 4;
+// const half = n => n / 2;
+// console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
+
+// Challenge 19
+function rating(arrOfFuncs, value) {
+  const numOfFuncs = arrOfFuncs.length;
+  let numOfTrue = 0;
+
+  for (const func of arrOfFuncs) {
+    if (func(value)) {
+      numOfTrue++;
+    }
+  }
+
+  return (numOfTrue / numOfFuncs) * 100;
+}
+
+// /*** Uncomment these to check your work! ***/
+const isEven = (n) => n % 2 === 0;
+const greaterThanFour = (n) => n > 4;
+const isSquare = (n) => Math.sqrt(n) % 1 === 0;
+const hasSix = (n) => n.toString().includes("6");
+const checks = [isEven, greaterThanFour, isSquare, hasSix];
+console.log(rating(checks, 64)); // should log: 100
+console.log(rating(checks, 66)); // should log: 75
+
+// Challenge 20
+function pipe(arrOfFuncs, value) {}
+
+// /*** Uncomment these to check your work! ***/
+// const capitalize = str => str.toUpperCase();
+// const addLowerCase = str => str + str.toLowerCase();
+// const repeat = str => str + str;
+// const capAddlowRepeat = [capitalize, addLowerCase, repeat];
+// console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+
+// Challenge 21
+function highestFunc(objOfFuncs, subject) {}
+
+// /*** Uncomment these to check your work! ***/
+// const groupOfFuncs = {};
+// groupOfFuncs.double = n => n * 2;
+// groupOfFuncs.addTen = n => n + 10;
+// groupOfFuncs.inverse = n => n * -1;
+// console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
+// console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
+// console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
+
+// Challenge 22
+function combineOperations(startVal, arrOfFuncs) {}
+
+function add100(num) {
+  return num + 100;
+}
+
+function divByFive(num) {
+  return num / 5;
+}
+
+function multiplyByThree(num) {
+  return num * 3;
+}
+
+// /*** Uncomment these to check your work! ***/
+// console.log(combineOperations(0, [add100, divByFive, multiplyByThree])); // Should output 60 -->
+// console.log(combineOperations(0, [divByFive, multiplyFive, addTen])); // Should output 10
+
+// Challenge 23
+function myFunc(array, callback) {}
+
+const numbers = [2, 3, 6, 64, 10, 8, 12];
+const evens = [2, 4, 6, 8, 10, 12, 64];
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+// /*** Uncomment these to check your work! ***/
+// console.log(myFunc(numbers, isOdd)); // Output should be 1
+// console.log(myFunc(evens, isOdd)); // Output should be -1
+
+// Challenge 24
+function myForEach(array, callback) {}
+
+let sum = 0;
+
+function addToSum(num) {
+  sum += num;
+}
+
+// /*** Uncomment these to check your work! ***/
+// const nums = [1, 2, 3];
+// myForEach(nums, addToSum);
+// console.log(sum); // Should output 6
