4f032d21c0d4c52b2203a53ae97b62bd9450ced6
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-03-08 19:25:09 +0100

    added vezbe29

--- vezbe29/app.js
+++ vezbe29/app.js
@@ -1,86 +1,168 @@
-// (function (a) {
-//   console.log(1);
-//   setTimeout(function () {
-//     console.log(2);
-//   }, 1000);
-//   setTimeout(function () {
-//     console.log(3);
-//   }, 0);
-//   console.log(4);
-// })();
-// //GET
-// //POST
-// //PUT
-// //DELETE
+// // (function (a) {
+// //   console.log(1);
+// //   setTimeout(function () {
+// //     console.log(2);
+// //   }, 1000);
+// //   setTimeout(function () {
+// //     console.log(3);
+// //   }, 0);
+// //   console.log(4);
+// // })();
+// // //GET
+// // //POST
+// // //PUT
+// // //DELETE
 
-// // JSON.stringify;-metoda
-// // JSON.parse;-metoda
+// // // JSON.stringify;-metoda
+// // // JSON.parse;-metoda
 
-// let str = JSON.stringify(nesto);
-// console.log(str);
-// console.log(typeof str);
+// // let str = JSON.stringify(nesto);
+// // console.log(str);
+// // console.log(typeof str);
 
-// // const json="{""""""objekat
-// const obj = (str = JSON.parse(json));
-// console.log(objekt);
+// // // const json="{""""""objekat
+// // const obj = (str = JSON.parse(json));
+// // console.log(objekt);
 
-// //PROMISI
+// // //PROMISI
 
-// // .try,.catch
+// // // .try,.catch
 
-// let p = new Promise((resolve, reject) => {
-//   let a = 2;
-//   if (a == 2) {
-//     resolve("Succes");
+// // let p = new Promise((resolve, reject) => {
+// //   let a = 2;
+// //   if (a == 2) {
+// //     resolve("Succes");
+// //   } else {
+// //     reject("Failed");
+// //   }
+// // });
+// // p.then((message) => {
+// //   console.log("This is in then" + message);
+// // }).catch((messsage) => {
+// //   console.log("This is in catch" + message);
+// // });
+
+// //////////////////////////////////////////////////////////////////////////////////////////7
+// let godine = 15;
+// let pro = new Promise((resolve, reject) => {
+//   if (godine >= 18) {
+//     resolve("Osoba je punoletna");
 //   } else {
-//     reject("Failed");
+//     reject("Osoba nije punoletna");
 //   }
 // });
-// p.then((message) => {
-//   console.log("This is in then" + message);
-// }).catch((messsage) => {
-//   console.log("This is in catch" + message);
+// pro
+//   .then((message) => {
+//     console.log("Status:" + message);
+//   })
+//   .catch((message) => {
+//     console.log("KUKU MAJKO:" + message);
+//   });
+
+// function wait() {
+//   return new Promise((resolve, rejects) => {
+//     setTimeout(() => {
+//       resolve("Proslo je 2 sekunde");
+//     }, 2000);
+//   });
+// }
+
+// wait().then((broj) => {
+//   console.log("Koliko je proslo?", broj);
 // });
 
-//////////////////////////////////////////////////////////////////////////////////////////7
-let godine = 15;
-let pro = new Promise((resolve, reject) => {
-  if (godine >= 18) {
-    resolve("Osoba je punoletna");
-  } else {
-    reject("Osoba nije punoletna");
-  }
-});
-pro
-  .then((message) => {
-    console.log("Status:" + message);
-  })
-  .catch((message) => {
-    console.log("KUKU MAJKO:" + message);
-  });
+// function izPozitivan(br) {
+//   return new Promise((resolve, reject) => {
+//     if (br > 0) {
+//       resolve(`Broj je pozitivan ${br}`); //bolje
+//     } else {
+//       reject("Broj nije pozitivan");
+//     }
+//   });
+// }
 
-function wait() {
-  return new Promise((resolve, rejects) => {
-    setTimeout(() => {
-      resolve("Proslo je 2 sekunde");
-    }, 2000);
-  });
-}
+// izPozitivan(5).then((broj) => {
+//   console.log(broj);
+// });
 
-wait().then((broj) => {
-  console.log("Koliko je proslo?", broj);
-});
+// async function mojaF() {
+//   return "Hello";
+// }
 
-function izPozitivan(br) {
-  return new Promise((resolve, reject) => {
-    if (br > 0) {
-      resolve(`Broj je pozitivan ${br}`); //bolje
-    } else {
-      reject("Broj nije pozitivan");
-    }
-  });
-}
+// mojaF().then((val) => {
+//   console.log(val);
+// });
+// function ispis() {
+//   console.log("Helloe");
+// }
+// ispis();
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 2000);
 
-izPozitivan(5).then((broj) => {
-  console.log(broj);
-});
+// async function myDisplay() {
+//   let myPromise = new Promise((resolve, reject) => {
+//     resolve("Ispisi nesto");
+//   });
+//   await myPromise.then((v) => {
+//     console.log(v);
+//   });
+// }
+
+// myDisplay();
+// const fetch = require("node-fetch");
+// //FETCH GET
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+//FETCH POST
+
+// post sluzi da dodamo neke podatke na serveru
+
+const data = {
+  title: "Nova obaveza",
+  completed: false,
+};
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+  method: "POST",
+  body: JSON.stringify(data),
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+// FETCH PUT
+
+const dataPUT = {
+  id: 1,
+  title: "izmenjena obaveza",
+  completed: true,
+};
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+  method: "PUT",
+  body: JSON.stringify(dataPUT),
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+const podaci = {
+  Ime: "Nisam ja",
+  Prezime: "neko drugo",
+  Skola: "Da",
+};
+fetch("https://run.mocky.io/v3/e8d794ad-aee6-4df4-91bb-d35954225d21")
+  .then((odgovor) => odgovor.json())
+  .then((pod) => console.log(pod));
+
+//FETCH DELETE
+
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+  method: "DELETE",
+}).then((response) => console.log("Odgovor sa statusom", response.status));
