b2461cb75cac08bd920798e471fe39b13f954ddf
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-03-17 20:29:38 +0100

    added element and checkbox

--- TODO lista/app.js
+++ TODO lista/app.js
@@ -1,12 +1,44 @@
-let unos = document.querySelector(".inp");
+// let unos = document.querySelector(".inp");
 
 let glavni = document.querySelector(".zadaci");
 
 function Dodaj() {
   let div = document.createElement("div");
   div.className = "item";
-
   glavni.appendChild(div);
+
+  // let check = document.createElement("input");
+  // check.type = "checkbox";
+  // glavni.appendChild(check);
 }
+const btn = document.querySelector("button");
+const unos = document.querySelector("input");
+const zadaci = document.querySelector(".zadaci");
+
+btn.addEventListener("click", () => {
+  if (unos.value === "") {
+    alert("Unesite nesto u input");
+  } else {
+    Dodaj();
+    let novielement = document.createElement("h3");
+    novielement.innerText = unos.value;
+    zadaci.appendChild(novielement);
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    zadaci.appendChild(check);
+    check.style.marginLeft = "50%";
+    check.style.marginTop = "-6.5%";
+    unos.value = "";
+    if (check.checked) {
+      novielement.style.color = "green";
+    }
+  }
+});
+check.addEventListener("change", function () {
+  let check = document.querySelector(".zadaci input[type=checkbox]");
+  if (check.checked) {
+    novielement.style.textDecoration = "line-through";
+  }
+});
 
 //domaci /teskt koji se dodaje na klik
