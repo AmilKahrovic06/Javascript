fcf01bfabc5d72fc7eaaad074d32d5036b5812a0
Amil Kahrovic <114291366+AmilKahrovic06@users.noreply.github.com>
2023-04-19 15:24:40 +0200

    added files by commit

---
+++ javascript-form/app.js
@@ -0,0 +1,24 @@
+const inp1 = document.querySelector(".input1");
+const inp2 = document.querySelector(".input2");
+const btn = document.querySelector("#btn");
+
+btn.onclick = () => {
+  const firstPassword = inp1.value;
+  const secondPassword = inp2.value;
+  if (firstPassword !== secondPassword) {
+    alert("Passwords do not match!");
+    inp2.value = "";
+  } else {
+    alert("Password is correct!");
+  }
+};
+
+// function check() {
+//   console.log("NESTO BILOSTA");
+//   if (firstPassword != secondPassword) {
+//     document.createElement("p");
+//     document.querySelector("p").innerHTML = "Passwords do not match";
+//   } else {
+//     alert("Password is correct");
+//   }
+// }
